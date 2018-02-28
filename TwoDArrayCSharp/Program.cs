49f9cab080ef802431dc9a1c29a372c0a51dc193
Nicholas Paul Sheppard <Nicholas.Sheppard@SingaporeTech.edu.sg>
2018-02-28 11:24:53 +0800

    Imported the TwoDArrayCSharp project.

---
+++ TwoDArrayCSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿//
+// ICT2106 Software Design - 2D array example
+//
+// Main program, C# version.
+//
+// Author: Nicholas Sheppard
+//
+
+using System;
+
+namespace ICT2106.TwoDArrayCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create and fill a 2 x 3 array of integers
+            TwoDArrayCSharp<int> array = new TwoDArrayCSharp<int>(2, 3);
+            array.Set(0, 0, 9); array.Set(0, 1, 4); array.Set(0, 2, 1);
+            array.Set(1, 0, 2); array.Set(1, 1, 5); array.Set(1, 2, 3);
+
+            // display the array in row major order
+            foreach (int n in array.GetRowMajorEnumerator())
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ TwoDArrayCSharp/TwoDArrayCSharp.cs
@@ -0,0 +1,59 @@
+﻿//
+// ICT2106 Software Design - 2D array example
+//
+// C# implementation of a two-dimensional array with a row-major iterator.
+//
+// Author: Nicholas Sheppard
+//
+using System.Collections;
+
+namespace ICT2106.TwoDArrayCSharp
+{
+    class TwoDArrayCSharp<T>
+    {
+        // the array
+        private T[][] array;
+
+        // constructor
+        public TwoDArrayCSharp(int rows, int columns)
+        {
+            // allocate memory for the array
+            array = new T[rows][];
+            for (int i = 0; i < rows; i++)
+                array[i] = new T[columns];
+        }
+
+        // get the element at (i, j)
+        public T Get(int i, int j)
+        {
+            return array[i][j];
+        }
+
+        // set the element at (i, j)
+        public void Set(int i, int j, T elem)
+        {
+            array[i][j] = elem;
+        }
+
+        // get an iterator that will traverse the array in row major order
+        public IEnumerable GetRowMajorEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+                for (int j = 0; j < array[i].Length; j++)
+                    yield return array[i][j];
+        }
+
+        // get the number of rows
+        public int Rows()
+        {
+            return array.Length;
+        }
+
+        // get the number of columns
+        public int Columns()
+        {
+            return (Rows() > 0)? array[0].Length : 0;
+        }
+
+    }
+}
